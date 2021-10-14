6d6358fa3016f5cd08a37ce07c4b6a5f96e190c0
Daniel-Avramovic <danijelgti@gmail.com>
2021-10-14 21:52:12 +0200

    implement redux

---
+++ src/state/reducers/userReducer.ts
@@ -0,0 +1,27 @@
+interface UserType {
+    page: number;
+    users: Array<object>[];
+    isLoading: boolean;
+    total: number;
+  };
+
+export const userReducer = (
+  state: UserType = { page: 1, users: [], isLoading: false, total: 0 },
+  action: any
+) => {
+  if (action.type === "setLoading") {
+    return { ...state, isLoading: action.isLoading };
+  }
+  if (action.type === "setPage") {
+    return { ...state, page: action.page };
+  }
+  if (action.type === "setUsers") {
+    return { ...state, users: action.users };
+  }
+  if (action.type === "setTotal") {
+    return { ...state, total: action.total };
+  }
+  return state;
+};
+
+//setLoading, setPage, setListOfUsers, setTotal;
